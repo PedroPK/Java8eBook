9316824550af80b3a65de4751af031f43a37c7a7
Pedro <pedropk@gmail.com>
2018-07-26 19:31:07 -0300

    First implementation of Sort example, at Chapter 05

--- tsw/java8/lambda/chapter05/Chapter05Test.java
+++ tsw/java8/lambda/chapter05/Chapter05Test.java
@@ -2,6 +2,7 @@ package java8.lambda.chapter05;
 
 import java.util.List;
 
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 import java8.lambda.chapter02.Chapter02;
@@ -11,9 +12,20 @@ public class Chapter05Test {
 	
 	@Test
 	public void ordenateUsers() {
-		List<Usuario> usersToBeSorted = ;
+		List<Usuario> unsortedList = Chapter02.getListaUsuarios();
+		List<Usuario> sortedList = Chapter05.sort(Chapter02.getListaUsuarios());
 		
-		Chapter05.sort(Chapter02.getListaUsuarios());
+		assertNotEquals(sortedList, unsortedList);
+		assertNotSame(sortedList, unsortedList);
+	}
+	
+	@Test
+	public void unsortedUsers() {
+		List<Usuario> unsortedList01 = Chapter02.getListaUsuarios();
+		List<Usuario> unsortedList02 = Chapter02.getListaUsuarios();
+		
+		assertEquals(		unsortedList02, unsortedList01);
+		assertNotSame(		unsortedList02, unsortedList01);
 	}
 	
 }
