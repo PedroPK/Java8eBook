480e5b83aa05c389143db5117173035493809822
pedropk <pedropk@gmail.com>
2018-09-01 11:58:15 -0300

    Renaming Classes and Methods

---
+++ tsw/java8/lambda/chapter02/Chapter02_LambdaExpressions_Test.java
@@ -0,0 +1,40 @@
+package java8.lambda.chapter02;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder(MethodSorters.JVM)
+public class Chapter02Test {
+	
+	@Test
+	public void testPrintNames() {
+		Chapter02_LambdaExpressions.printNames();
+	}
+	
+	@Test
+	public void testPrintNamesForEach() {
+		Chapter02_LambdaExpressions.printNamesForEach();
+	}
+	
+	@Test
+	public void testPrintNamesLambdaExtensive() {
+		Chapter02_LambdaExpressions.printNamesLambdaExtensive();
+	}
+	
+	@Test
+	public void testPrintNamesLambdaImplicitTypeWithBrackets() {
+		Chapter02_LambdaExpressions.printNamesLambdaImplicitTypeWithBrackets();
+	}
+	
+	@Test
+	public void testPrintNamesLambdaImplicitTypeWithoutBrackets() {
+		Chapter02_LambdaExpressions.printNamesLambdaImplicitTypeWithoutBrackets();
+	}
+	
+	@Test
+	public void testPrintNamesLambdaLean() {
+		Chapter02_LambdaExpressions.printNamesLambdaLean();
+	}
+	
+}
